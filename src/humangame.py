69c8a4db9f71d2de16b51aae1dc75eaf59cfba65
Flacox <el.flaco.fede+git@gmail.com>
2018-05-02 12:19:04 -0300

    add check_number function
    The function receives the guessed number and returns if it's equal, greater or lower than the number generated by the machine, when the guess is a number betweeen 1 and 100

--- src/humangame.py
+++ src/humangame.py
@@ -5,3 +5,16 @@ class HumanGame:
 
     def __init__(self):
         self.number = random.randint(1, 101)
+    
+    def check_number(self, guess):
+        if not guess.isnumeric():
+            raise ValueError("number should be between 1 and 100")
+        else:
+            guess_number = int(guess)
+            if guess_number < 1 or guess_number > 100:
+                raise ValueError("number should be between 1 and 100")
+            if guess_number == self.number:
+                return 0
+            elif guess_number < self.number:
+                return -1
+            return 1
--- test/test_humangame.py
+++ test/test_humangame.py
@@ -1,7 +1,27 @@
+import pytest
+
 from src.humangame import HumanGame
 
 class TestHumanGame:
 
-    def test_new_game_generates_number_between_1_and_100(self):
+    def setup_method(self):
         self.game = HumanGame()
+    
+    def test_new_game_generates_number_between_1_and_100(self):
         assert self.game.number >= 1 and self.game.number <= 100
+
+    def test_users_input_is_a_number_between_1_and_100(self):
+        result = self.game.check_number("50")
+        assert result == 0 or result == 1 or result == -1
+    
+    def test_number_lower_than_1_throws_ValueError(self):
+        with pytest.raises(ValueError):
+            self.game.check_number("0")
+            
+    def test_number_greater_than_100_throws_ValueError(self):
+        with pytest.raises(ValueError):
+            self.game.check_number("200")
+    
+    def test_string_input_throws_ValueError(self):
+        with pytest.raises(ValueError):
+            self.game.check_number("two")
