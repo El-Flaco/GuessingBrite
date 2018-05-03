d39f31b9ed582a65ac4bb08a4d61b4426a6dd38d
Flacox <el.flaco.fede+git@gmail.com>
2018-05-03 12:09:38 -0300

    fix welcome text

--- src/games.py
+++ src/games.py
@@ -1,5 +1,5 @@
-from src.humangame import HumanGame
-from src.computergame import ComputerGame
+from humangame import HumanGame
+from computergame import ComputerGame
 
 
 class Game:
@@ -9,8 +9,8 @@ class Game:
         self.select_mode()
         
     def select_mode(self):
-        print("\nWelcome to guessingBrite, the game where someone has to guess\
-                a number between 1 and 100")
+        print("\n__ Welcome to guessingBrite __")
+        print("\nIn this game someone has to guess a number between 1 and 100")
         print("\nThere are two game modes:\
                 \n\t[1] - You guess the number I thought.\
                 \n\t[2] - I guess the number you thought.")
@@ -24,3 +24,5 @@ class Game:
             self.mode = HumanGame()
         else:
             self.mode = ComputerGame()
+
+Game()
